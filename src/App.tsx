4d60cbd707f75ec9882e384770166840fa360d2c
amir <torabi@gmail.com>
2024-05-24 22:40:59 +0330

    add navs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotfound";
+import PageNavs from "./components/PageNavs";
+
+const routes = [
+  { path: "/", component: <HomePage /> },
+  { path: "home", component: <HomePage /> },
+  { path: "product", component: <Product /> },
+  { path: "pricing", component: <Pricing /> },
+];
 function App() {
   return (
     <>
-      <div>WorldWise</div>
+      <div>
+        <main>
+          <BrowserRouter>
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.component}
+                ></Route>
+              ))}
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </main>
+      </div>
     </>
   );
 }
