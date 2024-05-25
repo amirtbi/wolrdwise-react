86a68d99410a5225790ad3175b78e08bfe537d13
amir <torabi@gmail.com>
2024-05-25 22:49:35 +0330

    add auto-import-unplugin and define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product";
-import HomePage from "./pages/HomePage";
-import Pricing from "./pages/Pricing";
+import React from "react";
+import Product from "./pages/Product/Product";
+import HomePage from "./pages/Home/Home";
+import Pricing from "./pages/Pricing/Pricing";
 import PageNotFound from "./pages/PageNotfound";
+import AppLayout from "./pages/AppLayout/AppLayout";
+import Login from "./pages/Login/Login";
 import PageNavs from "./components/PageNavs";
+import { BrowserRouter } from "react-router-dom";
 
 const routes = [
   { path: "/", component: <HomePage /> },
-  { path: "home", component: <HomePage /> },
-  { path: "product", component: <Product /> },
-  { path: "pricing", component: <Pricing /> },
+  { path: "/home", component: <HomePage /> },
+  { path: "/product", component: <Product /> },
+  { path: "/pricing", component: <Pricing /> },
+  { path: "/app", component: <AppLayout /> },
+  { path: "/login", component: <Login /> },
 ];
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    AutoImport({
+      imports: ["react", "react-router-dom"],
+      dts: "./auto-imports.d.ts",
+      dirs: ["src", "src/pages", "src/components"],
+      eslintrc: {
+        enabled: true,
+      },
+      defaultExportByFilename: true,
+    }),
+  ],
+});
